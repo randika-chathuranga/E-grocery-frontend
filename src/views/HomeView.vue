784963892ba5d7602c9a0e09f1ec1f32f4b3724f
randika-chathuranga <randikachathuranga219@gmail.com>
2022-03-29 11:27:12 -0700

    added navbar basic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
-  <hello-world />
+  <v-app>
+    <Navbar />
+    <v-container>
+
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Navbar from '@/components/Navbar.vue'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      Navbar
     },
   }
 </script>
